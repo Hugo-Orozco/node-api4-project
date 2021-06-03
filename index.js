12f8a899379ec92d807377cbe6d62310611f72b6
Hugo-Orozco <aceasin@outlook.com>
2021-06-03 16:18:03 -0600

    Fixed Bugs

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // require your server and launch it
-require('dotenv').config()  ;
+require('dotenv').config();
 const server = require('./api/server');
+
+if (process.env.NODE_ENV === 'production') {
+    console.log('This means this code is deployed.');
+}
 
 const port = process.env.PORT || 5000;
 
 server.get('/api', (req, res) => {
-    res.json({ message: `API running on port ${process.env.PORT}.` });
+    res.json({ message: `API running on port ${port}.` });
 });
 
 server.use((req, res) => {
