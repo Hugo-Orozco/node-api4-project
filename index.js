7ec3892189d72d90ad0dd81c66e7be3b39e5afaa
Hugo-Orozco <aceasin@outlook.com>
2021-06-03 16:04:44 -0600

    API Page & 404 Page

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const server = require('./api/server');
 
 const port = process.env.PORT || 5000;
 
+server.get('/api', (req, res) => {
+    res.json({ message: `API running on port ${process.env.PORT}.` });
+});
+
+server.use((req, res) => {
+    res.status(404).json({ message: '404 Not Found' });
+});
+
 server.listen(port, () => {
     console.log(`Server listening on port ${port}.`);
 });
